972d1192bd92b85e6ee3315ee49dba666bf6a1ba
Peoples <67402756+LiterallyLink@users.noreply.github.com>
2025-10-20 18:48:51 -0700

    Init Commit.

---
+++ src/main/java/com/charmed/charmncraft/item/OrbVialItem.java
@@ -0,0 +1,51 @@
+package com.charmed.charmncraft.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class OrbVialItem extends Item {
+    private static final int CAPACITY = 1395; // 10 levels
+    private static final String NBT_KEY = "StoredXP";
+
+    public OrbVialItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        
+        if (world.isClient) {
+            return TypedActionResult.pass(stack);
+        }
+
+        int storedXP = stack.getOrCreateNbt().getInt(NBT_KEY);
+        
+        if (storedXP > 0) {
+            // Release XP
+            user.addExperience(storedXP);
+            stack.getOrCreateNbt().putInt(NBT_KEY, 0);
+            return TypedActionResult.success(stack);
+        }
+        
+        // Store XP
+        int playerXP = user.totalExperience;
+        int xpToStore = Math.min(playerXP, CAPACITY);
+        
+        if (xpToStore > 0) {
+            user.addExperience(-xpToStore);
+            stack.getOrCreateNbt().putInt(NBT_KEY, xpToStore);
+            return TypedActionResult.success(stack);
+        }
+        
+        return TypedActionResult.pass(stack);
+    }
+    
+    public int getCapacity() {
+        return CAPACITY;
+    }
+}
